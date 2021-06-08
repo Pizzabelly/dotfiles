2d6cb13b5e8b66971e0521b6b7d14ca6ce2fabae
Andrew Opalach <ado92300@gmail.com>
2021-06-08 11:52:34 -0400

    fix symlinks

--- .config/ncmpcpp/art.sh
+++ .config/ncmpcpp/art.sh
@@ -1,1 +1,88 @@
-/home/pizza/.config/ncmpcpp/art.sh
+#!/bin/zsh
+
+# Only works on kitty
+#
+# There is a bug where if the image or terminal window
+# are a certain size the image will scroll with the text
+#
+# Original:
+# https://github.com/alnj/dotfiles/tree/master/.ncmpcpp
+
+music_library=$HOME/music # mpd music library directory
+fallback="$HOME/.config/ncmpcpp/unknown.png" # image used if no cover is found
+
+percent_reserved_cols=30
+layout_reserved_rows=3
+
+font_height=22
+font_width=10
+
+main() {
+    kill_previous_instance_of_this_script
+    find_cover_image
+    display_cover_image
+    detect_window_resizes
+}
+
+kill_previous_instance_of_this_script() {
+    script_name=$(basename "$0")
+    for pid in $(pidof -x $script_name); do
+        if [ $pid != $$ ]; then
+            kill -15 $pid
+        fi
+    done
+}
+
+find_cover_image() {
+    album="$(mpc --format %album% current)"
+    file="$(mpc --format %file% current)"
+    album_dir="${file%/*}"
+    [ -z "$album_dir" ] && exit 1
+    album_dir="$music_library/$album_dir"
+    covers="$(find "$album_dir" -type d -exec find {} -maxdepth 1 -type f \
+      -iregex ".*/.*\(${album}\|cover\|folder\|artwork\|front\)[.]\\(jpe?g\|png\|gif\|bmp\)" \; )"
+    src="$(echo "$covers" | head -n1)"
+    if [ -z "$src" ]
+    then
+        covers="$(find "$album_dir" -type d -exec find {} -maxdepth 1 -type f \
+          -iregex ".*/.*\(${album}\|cover\|folder\|artwork\|front\).*[.]\\(jpe?g\|png\|gif\|bmp\)" \; )"
+        src="$(echo "$covers" | head -n1)"
+        [ -z "$src" ] && src=$fallback
+    fi
+}
+
+display_cover_image() {
+    unset LINES COLUMNS
+    term_rows=$(tput lines)
+    term_cols=$(tput cols)
+
+    reserved_cols=$((term_cols * (percent_reserved_cols / 100.0)))
+    reserved_rows=$((term_rows - layout_reserved_rows))
+
+    reserved_cols_scaled=$((reserved_cols / (font_height / font_width)))
+    reserved_rows_scaled=$((reserved_rows * (font_height / font_width)))
+
+    if [[ $reserved_cols_scaled -lt $reserved_rows ]]
+    then
+        kitty_width=$((reserved_cols))
+        kitty_height=$((reserved_cols_scaled))
+    else
+        kitty_width=$((reserved_rows_scaled))
+        kitty_height=$((reserved_rows))
+    fi
+
+    kitty_left=$(((reserved_cols / 2.0) - (kitty_width / 2.0)))
+    kitty_top=$((((term_rows / 2.0) - (kitty_height / 2.0)) + layout_reserved_rows - 1))
+
+    kitty +icat --clear --transfer-mode=file
+    kitty +icat --place=${kitty_width%.*}x${kitty_height%.*}@${kitty_left%.*}x${kitty_top%.*} --scale-up --transfer-mode=file "$src"
+}
+
+detect_window_resizes() {
+    {
+        trap 'display_cover_image' WINCH
+        while :; do sleep .1; done
+    } &
+}
+
+main >/dev/null 2>&1
--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,1 +1,34 @@
-/home/pizza/.config/nvim/init.vim
+set wrap!
+set number
+set signcolumn=yes
+set noshowmode
+
+let g:loaded_matchparen=1
+
+set nocompatible
+filetype plugin on
+syntax on
+
+set tabstop=4
+set shiftwidth=4
+set expandtab
+
+set autochdir
+set noshowmode
+
+let g:startify_font_height = 22.0
+let g:startify_font_width  = 10.0
+
+let g:startify_header_image = '/home/pizza/pics/emacs-start.png'
+
+let g:startify_lists = [
+      \ { 'type': 'files', 'header': [] },
+      \ ]
+
+call plug#begin('~/.vim/plugged')
+    Plug 'dylanaraps/wal.vim'
+    Plug 'file://'.expand('~/c/contrib/vim-startify')
+"    Plug 'mhinz/vim-startify'
+call plug#end()
+
+colo wal
